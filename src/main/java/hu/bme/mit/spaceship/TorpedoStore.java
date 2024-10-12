e46b2294ee720c9b9c09a2df3de893be2011196b
Gema Csanád <gemacsanad@gmail.com>
2024-10-13 00:31:14 +0200

    Fixed torpedoCount

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -45,7 +45,7 @@ private  Random generator = new Random();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
